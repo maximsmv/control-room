8b6d483d7b0c8e9b66fb9bba26214c3eb7831c17
maximsmv <maximsmv27@gmail.com>
2023-02-09 12:43:15 +0500

    commit

---
+++ common/src/test/java/com/itprom/jet/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itprom.jet.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
